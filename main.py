0a32df5f43803591b9e10c34d7aa409276bf9736
mbilalhameed <mbilal.hameed@outlook.com>
2022-06-19 22:23:04 +0500

    Implemented post request and processed data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -6,3 +6,18 @@ app = FastAPI()
 @app.get('/')
 async def root():
     return {'message': "Hello world!"}
+
+
+@app.get('/posts')
+def get_posts():
+    return {'data': ['post1', 'post2', 'post3']}
+
+
+@app.post('/createpost')
+def create_post(pay_load: dict = Body(...)):
+    print(pay_load)
+    return {
+        'message': "post created successfully",
+        "post_title": f"{pay_load['title']}",
+        "post_content": f"{pay_load['content']}"
+    }
