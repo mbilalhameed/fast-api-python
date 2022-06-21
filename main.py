ab292d3aba402ba2f0a5cea1f51d38c9a5031b42
mbilalhameed <mbilal.hameed@outlook.com>
2022-06-21 20:59:46 +0500

    Validations with Fast API and Understanding of API response object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -14,6 +14,25 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+def find_post(post_id):
+    for post in my_posts:
+        if post['id'] == post_id:
+            return post
+
+
+def remove_post(post_id):
+    if not my_posts:
+        return None, False
+
+    for index, post in enumerate(my_posts):
+        if post['id'] ==post_id:
+            my_posts.pop(index)
+
+            return post, True
+
+    return None, False
+
+
 @app.get('/')
 async def root():
     return {'message': "Hello world!"}
@@ -21,10 +40,10 @@ async def root():
 
 @app.get('/posts')
 def get_posts():
-    return {'data': ['post1', 'post2', 'post3']}
+    return {'data': my_posts}
 
 
-@app.post('/post')
+@app.post('/post', status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
     post = post.dict()
     post['id'] = len(my_posts) + 1
@@ -32,6 +51,46 @@ def create_post(post: Post):
     my_posts.append(post)
 
     return {
-        'message': "post created successfully",
+        'message': "Post created successfully",
         "post": post
     }
+
+
+@app.get('post/latest')
+def get_latest_post():
+    if my_posts:
+        post = my_posts[-1]
+
+        return {
+            "message": "Latest post fetched successfully",
+            "post": post
+        }
+
+    else:
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"There are no posts")
+
+
+@app.get('/post/{id}')
+def get_post(post_id: int):
+    post = find_post(post_id)
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id={post_id} doesn't exist'")
+
+    return {
+        "message": "Post fetched successfully",
+        "post": post
+    }
+
+
+@app.delete('post/delete/{id}', status.HTTP_204_NO_CONTENT)
+def delete_post(post_id: int):
+    post, removed = remove_post(post_id)
+
+    if not removed:
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
